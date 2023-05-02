7ce2ff67cd5141a253f01ca8d9426e3ea1e86bf6
Ethan Ayers <ethan.ayers88@gmail.com>
2023-05-02 13:28:10 -0600

    Added save to notes

--- books-final/src/components/Notepad.jsx
+++ books-final/src/components/Notepad.jsx
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 
 const Notepad = () => {
   const [notes, setNotes] = useState('');
+  const [savedNotes, setSavedNotes] = useState([]);
 
   const handleChange = (event) => {
     setNotes(event.target.value);
   };
 
+  const handleSave = (event) => {
+    setSavedNotes(event.target.value);
+  }
+
   return (
     <div>
       <h1>Notepad</h1>
       <textarea rows="20" cols="50" value={notes} onChange={handleChange} />
+      <div>
+        <button onClick={handleSave}>Save Note</button>
+      </div>
     </div>
   );
 };
